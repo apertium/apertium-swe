cbc5594ba3d4f0afd85b18a3675cc6f55150526e
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2015-12-11 14:06:00 +0000

    fullform saldo downloader

---
+++ dev/saldo/fullform/download.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+count=0;
+for i in `cat swe-words-paradigms.txt | tr ' ' '$' | tr '\t' ';'`; do
+	ord=`echo $i | cut -f1 -d';' | tr '$' ' '`;
+	ordklass=`echo $i | cut -f2 -d';' | tr '$' ' '`;
+        # echo "${ord}"
+        # echo "${ordklass}"
+        # echo "${count}"
+        if ! test -s "paradigms/$ordklass/$ord.xml"; then
+            printf "%06d ||| %s ||| %s\n" "$count" "$ord" "$ordklass"
+            wget -q "http://spraakbanken.gu.se/ws/saldo-ws/gen/xml/$ordklass/$ord" -O "paradigms/$ordklass/$ord.xml";
+        fi
+	count=`expr $count + 1`;
+done
